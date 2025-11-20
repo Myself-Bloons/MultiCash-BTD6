4ed19fae532b145943755ff370144c5ad0fa3b73
Myself <38400384+Myself-Bloons@users.noreply.github.com>
2025-11-20 15:18:22 +0100

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiCash;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "51.2";
+    public const string Version = "1.0.0";
+    public const string Name = "MultiCash";
+    public const string Description = "Allows you to Multiply the Cash gained from ANY Source, the global Multiplier counts towards any allowed source of income, you can also go below 1 to make things harder. All credits go to GMFelixfex.";
+    public const string RepoOwner = "Myself-Bloons";
+    public const string RepoName = "MultiCash-BTD6";
+    public const string Author = "Myself";
+    public const string DllName = "MultiCash.dll";
+    public const string Icon = "Icon.png";
+}
