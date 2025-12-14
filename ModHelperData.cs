4259f90ec4fb43c2eea4592c284fd8401d2b9929
Myself <38400384+Myself-Bloons@users.noreply.github.com>
2025-12-14 12:04:08 +0100

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@
 public static class ModHelperData
 {
     public const string WorksOnVersion = "52.2";
-    public const string Version = "1.0.0";
+    public const string Version = "1.1.0";
     public const string Name = "MultiCash";
     public const string Description = "Allows you to Multiply the Cash gained from ANY Source, the global Multiplier counts towards any allowed source of income, you can also go below 1 to make things harder. All credits go to GMFelixfex.";
     public const string RepoOwner = "Myself-Bloons";
